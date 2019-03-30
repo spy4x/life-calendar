45afd2099a7c64457499830cb717ea7237e835e9
Dmitriy <dmitriy.kolesov@gmail.com>
2019-03-30 14:25:38 +0300

    get position by navigation

--- src/app/services/country/country.service.ts
+++ src/app/services/country/country.service.ts
@@ -7,11 +7,39 @@ export class CountryService {
 
   constructor(private http: HttpClient) { }
 
+  username = 'dmitriy.kolesov';
+
   async get(): Promise<string> {
-    if (this.country) {
-      return this.country;
-    }
-    const response = await this.http.get<{country: string}>('http://ip-api.com/json').toPromise();
-    return this.country = response.country;
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (this.country) {
+          resolve(this.country);
+        }
+        const response = await this.http.get<{ country: string }>('http://ip-api.com/json').toPromise();
+        if (response.country) {
+          this.country = response.country;
+          resolve(this.country);
+         }
+        if (navigator.geolocation) {
+
+          await
+            navigator.geolocation.getCurrentPosition(async (position) => {
+              const data: any = await this.http.get('http://api.geonames.org/countryCode', {
+                params: {
+                  lat: `${position.coords.latitude}`,
+                  lng: `${position.coords.longitude}`,
+                  type: 'JSON',
+                  username: this.username,
+                },
+              })
+                .toPromise();
+              this.country = data.countryName;
+              resolve(this.country);
+            });
+        }
+      } catch (e) {
+        reject(e);
+      }
+    });
   }
 }
