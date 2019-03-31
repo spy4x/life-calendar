bc3a2f6446c2a8571d01134e7c558524e5138748
Anton Shubin <2spy4x@gmail.com>
2019-03-31 12:39:33 +0300

    feat(speech): choose voice and language

--- src/app/services/speech/speech.service.ts
+++ src/app/services/speech/speech.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 
 const synth = window.speechSynthesis;
+const responsiveVoice = (window as any).responsiveVoice;
 
 @Injectable()
 export class SpeechService {
+  private voices: SpeechSynthesisVoice[];
+  private voice: SpeechSynthesisVoice;
 
-  constructor() {
+  getVoice() {
+    if (!this.voice) {
+      this.voices = synth.getVoices().filter(v => v.lang.startsWith('en'));
+      const danielVoice = this.voices.find(v => (v.voiceURI.toLowerCase().indexOf('daniel') >= 0) && v.lang === 'en-GB');
+      const anyEnglishVoice = this.voices.find(v => v.lang.startsWith('en'));
+      this.voice = danielVoice || anyEnglishVoice || this.voices[0];
+    }
+    return this.voice;
   }
 
   async speak(text: string): Promise<void> {
@@ -16,13 +26,14 @@ export class SpeechService {
       synth.cancel();
     }
     return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(), 4000); // fallback
+      setTimeout(() => resolve(), 4000); // fallback, because sometimes bug happens - "onend" is not called at all
       try {
         const utterance = new SpeechSynthesisUtterance(text);
         utterance.onend = () => resolve();
         utterance.onerror = error => reject(error);
         utterance.pitch = 1;
         utterance.rate = 1;
+        utterance.voice = this.getVoice();
         synth.speak(utterance);
       } catch (error) {
         reject(error);
